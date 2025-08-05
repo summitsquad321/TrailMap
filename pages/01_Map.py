df1fc6e1b21bd8579b5315d538c07d4cbdebf8b4
summitsquad321 <summitsquad321@gmail.com>
2025-08-05 13:08:19 -0400

    Update 01_Map.py

--- pages/01_Map.py
+++ pages/01_Map.py
@@ -152,7 +152,7 @@ deck = pdk.Deck(
     initial_view_state  = view_state,
     tooltip             = tooltip,
     map_provider        = "mapbox",
-    map_style           = "mapbox://styles/mapbox/outdoors-v12",
+    map_style           = "mapbox://styles/mapbox/outdoors-v11",
 )
 
 st.pydeck_chart(deck, use_container_width=True)
